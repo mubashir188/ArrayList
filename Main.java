119881ebf8018780c294520587750013e76d5186
mubashir188 <75080563+mubashir188@users.noreply.github.com>
2020-12-03 19:04:40 +0500

    My First Commit

---
+++ Main.java
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	
+	int[] arr = new int[5];
+	List lst = new ArrayList();
+    lst.add(123);
+    lst.add(234);
+    lst.add(125);
+    
+    for(int i = 0; i < lst.size() ; i++)
+    {
+    System.out.println(lst.get(i));
+        
+    }
+}}
